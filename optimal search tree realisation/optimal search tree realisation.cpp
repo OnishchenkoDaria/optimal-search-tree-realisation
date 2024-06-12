d8dc1b18b023d697c07fac860b5b47dd1b103152
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2024-06-12 23:20:37 +0300

    making it more class-based structure

--- optimal search tree realisation/optimal search tree realisation.cpp
+++ optimal search tree realisation/optimal search tree realisation.cpp
@@ -3,7 +3,7 @@
 #include <limits>
 using namespace std;
 
-// rebuilt rational digit struct as a class
+//rebuilt rational digit struct as a class
 class Rational {
 private:
     int numerator;
@@ -19,119 +19,101 @@ public:
         this->numerator = num;
     }
 
-    void setDenom(int num)
-    {
+    void setDenom(int num) {
         this->denominator = num;
     }
 
-    int getNumer() {
+    int getNumer() const {
         return this->numerator;
     }
 
-    int getDenom() {
+    int getDenom() const {
         return this->denominator;
     }
 };
 
-//setting static values and algorythms as a separate class
+//setting static values and algorithms as a separate class
 class OptimalBST {
 private:
+    vector<vector<int>> t;
+    vector<vector<int>> root;
+    vector<int> sum;
+
+public:
     static const int MAX = 300;
     static const int INF = numeric_limits<int>::max();
 
-};
+    OptimalBST() : t(MAX, vector<int>(MAX, INF)), root(MAX, vector<int>(MAX, -1)), sum(MAX, 0) {}
 
-//max range of  the array
-#define MAX 300
-//initialization of the infinity
-#define INF numeric_limits<int>::max()
-
-//пробна вибірка для тестування
-
-int sum[MAX];
-//t & root: 2-dimensional vectors used for storing 
-//results of the calculations and roots of optimal subtree
-int t[MAX][MAX];
-int root[MAX][MAX];
-
-//calculates the weight of the subtree with vertexes i & j
-int weight(int i, int j, vector<Rational> m) {
-    if (i > j) return 0;
-    if (i > 0) {
+    //calculates the weight of the subtree with vertices i & j
+    int weight(int i, int j, const vector<Rational>& m) {
+        if (i > j) return 0;
         int total = 0;
-        for (int k = i - 1; k <= j - 1; k++) {
+        for (int k = i; k <= j; k++) {
             total += m[k].getNumer() * m[k].getDenom();
         }
         return total;
     }
-    else {
-        return 0;
-    }
-}
 
-//recursively gose through al possible combinations of subtrees and saves results at and root
-int go(int i, int j, vector<Rational> m) {
-    if (i > j) return 0;
-    if (t[i][j] == INF) {
-        for (int k = i; k <= j; k++) {
-            int temp = go(i, k - 1, m) + go(k + 1, j, m) + weight(i, k - 1, m) + weight(k + 1, j, m);
-            if (temp < t[i][j]) {
-                t[i][j] = temp;
-                root[i][j] = k;
+    //recursively goes through all possible combinations of subtrees and saves results at t and root
+    int go(int i, int j, const vector<Rational>& m) {
+        if (i > j) return 0;
+        if (t[i][j] == INF) {
+            for (int k = i; k <= j; k++) {
+                int temp = go(i, k - 1, m) + go(k + 1, j, m) + weight(i, k - 1, m) + weight(k + 1, j, m);
+                if (temp < t[i][j]) {
+                    t[i][j] = temp;
+                    root[i][j] = k;
+                }
             }
         }
+        return t[i][j];
     }
-    return t[i][j];
-}
 
-//prints root/-s of optimal subtree
-void printOptimalBST(int i, int j, int parent, vector<Rational> m) {
-    if (i <= j) {
-        int k = root[i][j];
-        cout << "Root: " << m[k - 1].getNumer() << endl;
+    //print the structure of the optimal BST
+    void printOptimalBST(int i, int j, const vector<Rational>& m) {
+        if (i <= j) {
+            int k = root[i][j];
+            cout << "Root: " << m[k].getNumer() << endl;
+        }
     }
-}
 
-int main() {
-    //first selection
-    vector<Rational> m = {
-    {4, 1},
-    {10, 1},
-    {6, 1},
-    {2, 1},
-    {3, 1},
-    {5, 1},
-    {7, 1}
+    void initialize(const vector<Rational>& m) {
+        int n = m.size();
+        for (int i = 0; i < n; i++) {
+            fill(t[i].begin(), t[i].end(), INF);
+            fill(root[i].begin(), root[i].end(), -1);
+        }
+        sum[0] = 0;
+        for (int i = 0; i < n; i++) {
+            sum[i + 1] = sum[i] + m[i].getNumer() * m[i].getDenom();
+            t[i][i] = 0;
+        }
+    }
+
+    void test(const vector<Rational>& m, int num_threads) {
+        initialize(m);
+        int n = m.size() - 1;
+
+        int optimal_cost = go(0, n, m);
+        cout << "Optimal cost of BST: " << optimal_cost << endl;
+        printOptimalBST(0, n, m);
+    }
+};
+
+void runTests() {
+    OptimalBST bst;
+
+    vector<Rational> m1 = {
+        {4, 1},
+        {10, 1},
+        {6, 1},
+        {2, 1},
+        {3, 1},
+        {5, 1},
+        {7, 1}
     };
 
-    int n = m.size();
-
-    //initiallize t array
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            t[i][j] = INF;
-        }
-    }
-
-    //initiallize root array
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            root[i][j] = -1;
-        }
-    }
-
-    sum[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        t[i][i] = 0;
-        sum[i] = sum[i - 1] + m[i - 1].getNumer() * m[i - 1].getDenom();
-    }
-
-    int optimal_cost = go(1, n, m);
-    cout << "Optimal cost of BST: " << optimal_cost << endl;
-
-    printOptimalBST(1, n, 0, m);
-
-    //second selection
     vector<Rational> m2 = {
         {4, 1},
         {10, 1},
@@ -142,30 +124,10 @@ int main() {
         {7, 5}
     };
 
-    n = m2.size();
+    bst.test(m1, 4);
+    bst.test(m2, 4);
+}
 
-    //initiallize t array
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            t[i][j] = INF;
-        }
-    }
-
-    //initiallize root array
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < MAX; j++) {
-            root[i][j] = -1;
-        }
-    }
-
-    sum[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        t[i][i] = 0;
-        sum[i] = sum[i - 1] + m2[i - 1].getNumer() * m2[i - 1].getDenom();
-    }
-
-    int optimal_cost2 = go(1, n, m2);
-    cout << "Optimal cost of BST: " << optimal_cost2 << endl;
-
-    printOptimalBST(1, n, 0, m2);
+int main() {
+    runTests();
 }
